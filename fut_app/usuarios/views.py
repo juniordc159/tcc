701c23629f2f3f234ae2fdc680aff88cc688502f
juniordc159 <juniordc1599@gmail.com>
2025-04-25 23:30:52 -0300

    fix some things

--- fut_app/usuarios/views.py
+++ fut_app/usuarios/views.py
@@ -8,20 +8,17 @@ from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
-        # Usando email como username
-        username = request.POST.get('username')  # <- nome do campo no HTML
-        senha = request.POST.get('password') # <- nome do campo no HTML
+        username = request.POST.get('username')  
+        senha = request.POST.get('password') 
 
-        # Autenticar o usuário usando o campo 'username' (que é o email)
         user = authenticate(request, username=username, password=senha)
 
         if user is not None:
-            # Se o usuário for encontrado, realiza o login
             login(request, user)
-            return redirect('pagina_principal')  # Redirecionar para a página principal
+            return redirect('pagina_principal')  
         else:
             messages.error(request, 'Credenciais inválidas.')
-            return redirect('login')  # Redirecionar de volta para o login
+            return redirect('login')  
 
     return render(request, 'usuarios/login.html')
 
@@ -39,20 +36,15 @@ def cadastro_view(request):
         senha = request.POST.get('password')
         confirmar_senha = request.POST.get('password_confirmation')
 
-        # Verificação se as senhas são iguais
         if senha != confirmar_senha:
             messages.error(request, 'As senhas não coincidem.')
             return redirect('cadastro')
 
-        # Verificar se o email já está em uso
         if User.objects.filter(username=email).exists():
             messages.error(request, 'Este email já está em uso.')
             return redirect('cadastro')
 
-        # Criando o usuário no banco de dados
         user = User.objects.create_user(username=email, email=email, password=senha)
-
-        # Login automático após cadastro
         login(request, user)
 
         # Redirecionar para a página principal após o cadastro
